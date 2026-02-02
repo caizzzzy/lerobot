9a3d9aaecef71c5682edb1ebb1126c9b2b8a131f
Zilong Huang <chongchongjjj@gmail.com>
2026-02-02 17:56:57 +0800

    initial commit3

--- src/lerobot/configs/default.py
+++ src/lerobot/configs/default.py
@@ -35,6 +35,7 @@ class DatasetConfig:
     use_imagenet_stats: bool = True
     video_backend: str = field(default_factory=get_safe_default_codec)
     streaming: bool = False
+    local_files_only: bool = False#加上
 
 
 @dataclass
